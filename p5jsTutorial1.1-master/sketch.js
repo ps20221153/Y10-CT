8c8d13d0031c6670beecdd893110375fa520218c
ps20221153 <46880602+ps20221153@users.noreply.github.com>
2019-01-23 11:45:40 +0800

    Add files via upload

--- p5jsTutorial1.1-master/sketch.js
+++ p5jsTutorial1.1-master/sketch.js
@@ -1,19 +1,29 @@
 function setup()
 {
   //create a drawing service 700px wide, 500px tall
-  createCanvas(300,300);
+  createCanvas(677,677);
 
 }
 
 function draw()
 {
   //create a background using RGB values
-  background(10,10,10)
+  //I worked with Joycelyn to give my background a salmon colour
+  background(100,128,114)
 
   // change the paint brush to a specific color according to RGB values
-  fill(204,204,355);
+  fill(124,252,0);
 
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
-  rect(20,20,20,20);
+  rect(300,300,200,200);
+
+  rect(400,400,200,200);
+
+  rect(10,10,200,200);
+
+  rect(460,10,200,200);
+
+
+
 
 }

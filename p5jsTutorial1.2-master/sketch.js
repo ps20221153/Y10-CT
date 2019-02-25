edde7c10f3af95589cccdd860e3d849f3f723358
ps20221153 <46880602+ps20221153@users.noreply.github.com>
2019-02-25 13:08:02 +0800

    Add files via upload

--- p5jsTutorial1.2-master/sketch.js
+++ p5jsTutorial1.2-master/sketch.js
@@ -1,34 +1,34 @@
 function setup()
 {
-  createCanvas(700,500);
-  
+  createCanvas(500,500);
+
 }
 
 function draw()
 {
-	background(255,0,0)
+	background(100,100,100)
 
 	// change the paint brush to a specific color according to RGB values
 	// this color applies only to the inside color of a shape
-	fill(0,0,255);
+	fill(0,255,255);
 
 	// change the paint brush to a specific color according to RGB values
 	// this color applies only to the outside color of a shape
-	stroke(0,0,0);
+	stroke(0,255,255);
 
 	// modifies the thickness of the border of an object
-	strokeWeight(3);
+	strokeWeight(1);
 
 
 	rect(10,10,100,50);
 	rect(10,100,100,50);
 
 
-	fill(50,255,20);    
-	stroke(255,255,255);
+	fill(50,255,20);
+	stroke(0,255,255);
 	strokeWeight(10);
 
 	rect(150,10,100,50);
-	rect(150,100,100,50);	
+	rect(150,100,100,50);
 
 }

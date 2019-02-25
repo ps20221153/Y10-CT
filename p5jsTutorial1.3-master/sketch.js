12a0610627922916180d510f6a45bbb8e500cd3d
ps20221153 <46880602+ps20221153@users.noreply.github.com>
2019-02-25 12:37:28 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master/sketch.js
@@ -0,0 +1,27 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(255,0,0)
+
+	fill(0,0,255);
+	stroke(0,0,0);
+	strokeWeight(3);
+
+	text("Hello World",300,490);
+
+	rect(250,270,200,200);
+
+	text("Charles Yang",300,40);
+
+	rect(250,50,200,200);
+
+	fill(51,255,51);
+
+	text("",300,10);
+
+}
